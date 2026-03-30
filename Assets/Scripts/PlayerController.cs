d9a9583f8d6d71db4c1cdbfacba6a30bde0d3d89
Mothburger <127431557+Mothburger@users.noreply.github.com>
2026-03-30 14:37:27 -0400

    Added Acceleration

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,10 @@ public class PlayerController : MonoBehaviour
     GameObject PlayerGun;
     PhotonView photonView;
     [SerializeField]
-    float MovementSpeed = 20.0f;
+    float StartingMovementSpeed = 0.0f;
+    [SerializeField]
+    float MaxMovementSpeed = 25.0f;
+    float MovementSpeed = 0.0f;
     [SerializeField]
     float TankRotationSpeed = 20.0f;
     [SerializeField]
@@ -31,8 +34,18 @@ public class PlayerController : MonoBehaviour
     }
         if (Input.GetKey(KeyCode.W))
         {
+            MovementSpeed +=0.01f;
+            if (MovementSpeed == MaxMovementSpeed)
+            {
+                MovementSpeed = MaxMovementSpeed;
+            }
             transform.position += (transform.up * MovementSpeed * Time.deltaTime);
+            
         }
+        if (Input.GetKeyUp(KeyCode.W))
+            {
+            MovementSpeed = StartingMovementSpeed;
+            }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(0,0, TankRotationSpeed * Time.deltaTime); 
@@ -53,6 +66,7 @@ public class PlayerController : MonoBehaviour
         {
             PlayerGun.transform.Rotate(0,0, -GunRotationSpeed * Time.deltaTime); 
         }
+        
     
     }
 }
